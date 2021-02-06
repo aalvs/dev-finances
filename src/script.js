35a9fe91ac0b68ed25acdc29a7095bc486603509
Andre Alves <andrealvesdeoliveira@outlook.com>
2021-02-06 11:46:20 -0300

    Configurado os metodos para formatação dos valores

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ const Modal = {
     // }
 }
 
-const transactions = [
+const transactions = [ 
     {
         id: 1,
         description: 'Luz',
@@ -28,54 +28,149 @@ const transactions = [
     {
         id: 2,
         description: 'Criação de website',
-        amount: 5000000,
+        amount: 500000,
         date: '23/01/2021',
     },
     {
         id: 3,
         description: 'Internet',
-        amount: 20000,
+        amount: -20000,
+        date: '23/01/2021',
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 50000,
         date: '23/01/2021',
     }
 ]
     
 const Transaction = {
+    all: transactions,
+
+    add(transaction) {
+        Transaction.all.push(transaction);
+        App.reload();
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1);
+        App.reload();
+    },
+
     incomes() {
+        let income = 0;
 
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount > 0) {
+                income += transaction.amount
+            }
+        })
+        return income; 
     },
+
     expenses() {
+        let expense = 0;
 
+        Transaction.all.forEach((transaction) => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })
+        return expense;
     },
-    total() {
 
+    total() {
+        let total = Transaction.incomes() + Transaction.expenses();
+        return total;
     }
 }
 
 // Document Object Model
-const DOM = {
+const DOM = { 
     transactionsContainer: document.querySelector('#data-table tbody'),
 
     addTransaction(transaction, index) {
-        const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        console.log(transaction)
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
 
-        DOM.transactionsContainer.appendChild(tr)
+        DOM.transactionsContainer.appendChild(tr);
     },
+
     innerHTMLTransaction(transaction, index) {
-        
+        const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = ` 
         <td class="description">${transaction.description}</td>
-        <td class="expense">${transaction.amount}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
             <img src="../assets/minus.svg" alt="">
         </td>
         `
         return html
-    }
+    },
 
+    updateBalance() {
+        document.getElementById('incomeDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.incomes());
+
+        document.getElementById('expenseDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.expenses());
+
+        document.getElementById('totalDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.total());
+    },
+    cleaTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
+    }
 }
 
-transactions.forEach(function (transaction) {
-    DOM.addTransaction(transactions)
-})
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        })
+        return signal + value
+    }
+}
+
+const Form = {
+    submit(e) {
+        e.preventDefault();
+
+        
+    }
+}
+
+const App = {
+    init() {
+
+        // DOM.addTransaction(transactions[0]);
+        // DOM.addTransaction(transactions[1]);
+        // DOM.addTransaction(transactions[2]);
+
+        Transaction.all.forEach((transaction) => {
+            DOM.addTransaction(transaction);
+        })
+
+        DOM.updateBalance();
+    },
+    reload() {
+        DOM.cleaTransactions();
+        App.init();
+    }
+}
+
+App.init();
+
